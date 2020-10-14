f634fea1c1dc8f43ed97d07f06bcfe18e1a37f97
Diego Rojas <darguelles.rojas91@gmail.com>
2020-10-14 17:33:40 -0300

    Add java baseline project

---
+++ java-oidc/src/main/java/com/blitzrok/demo/javaoidc/JavaOidcApplication.java
@@ -0,0 +1,13 @@
+package com.blitzrok.demo.javaoidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOidcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOidcApplication.class, args);
+	}
+
+}
---
+++ java-oidc/src/test/java/com/blitzrok/demo/javaoidc/JavaOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blitzrok.demo.javaoidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
