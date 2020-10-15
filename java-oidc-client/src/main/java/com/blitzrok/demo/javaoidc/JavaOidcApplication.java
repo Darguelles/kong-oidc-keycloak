50ac8605d0b8dbcd38439fb2f60e7ce23cc05fc3
Diego Rojas <darguelles.rojas91@gmail.com>
2020-10-15 02:46:28 -0300

    Java app gets kong resource

--- java-oidc-client/src/main/java/com/blitzrok/demo/javaoidc/JavaOidcApplication.java
+++ java-oidc-client/src/main/java/com/blitzrok/demo/javaoidc/JavaOidcApplication.java
@@ -2,6 +2,8 @@ package com.blitzrok.demo.javaoidc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JavaOidcApplication {
--- java-oidc-client/src/main/java/com/blitzrok/demo/javaoidc/controller/ClientController.java
+++ java-oidc-client/src/main/java/com/blitzrok/demo/javaoidc/controller/ClientController.java
@@ -4,12 +4,15 @@ import com.blitzrok.demo.javaoidc.controller.dto.AuthenticationInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 
 @RestController
@@ -20,17 +23,14 @@ public class ClientController {
     @Autowired
     private OAuth2AuthorizedClientService authorizedClientService;
 
+    private RestTemplate restTemplate = new RestTemplate();
+
     @GetMapping("/home")
     public ResponseEntity Home() {
         return ResponseEntity.ok("Welcome to /home endpoint");
     }
 
-    @GetMapping("/auth")
-    public ResponseEntity Auth() {
-        return ResponseEntity.ok("This is the auth endpoint");
-    }
-
-    @GetMapping("/userinfo")
+    @GetMapping("/auth-info")
     public String userinfo(OAuth2AuthenticationToken authentication) {
         OAuth2AuthorizedClient authorizedClient =
                 this.authorizedClientService.loadAuthorizedClient(
@@ -40,5 +40,20 @@ public class ClientController {
                 authorizedClient.getRefreshToken().getTokenValue());
         log.info(info.toString());
         return "ok";
+    }
+
+    @GetMapping("/get-mocks")
+    public String GetMocks(OAuth2AuthenticationToken authentication) {
+        OAuth2AuthorizedClient authorizedClient =
+                this.authorizedClientService.loadAuthorizedClient(
+                        authentication.getAuthorizedClientRegistrationId(),
+                        authentication.getName());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer "+ authorizedClient.getAccessToken().getTokenValue());
+        log.info("Authorization token set: " + authorizedClient.getAccessToken().getTokenValue());
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        String result = restTemplate.getForObject("http://localhost:8000/mock", String.class, entity);
+        return result;
     }
 }
